4d534ab34de2849a32faa08576313dfa0bc3db40
Corey Grandmaison <cgsustainability@gmail.com>
2019-08-13 16:00:52 -0400

    html v1 done, css v1, js still being built

--- public/main.js
+++ public/main.js
@@ -1,13 +1,21 @@
 const getWeather = () => {
-  console.log('getting the weather')
+  console.log("getting the weather");
   //get the user input
-  const query = document.querySelector('#searchQuery').value 
+
+  fetch(
+    "http://api.openweathermap.org/data/2.5/weather?q=tampa&appid=93ba71da97ad4cd92c7c9ea8576ae9f6"
+  ).then(response => {
+    console.log(response);
+    document.querySelector(".city-conditions").textContent;
+    document.querySelector("city-temp").textContent;
+  });
+
+  const query = document.querySelector("#searchQuery").value;
 
   //send api call
   // output to the screen
+};
 
-}
-
-
-document.querySelector('.search-button').addEventListener('click', getWeather)
-
+document //grab the search button, on click, search weather
+  .querySelector(".search-button")
+  .addEventListener("click", getWeather);
